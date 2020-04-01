a0236a21089ec58b5cedb006464e9296e8c4e4b5
AMirshekari <amirshekari@ebates.com>
2020-04-01 13:24:29 -0700

    Updates

---
+++ precache-manifest.34917f3a4fa281f8dc1811b04773a02a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41f59fa5291f9086d91d45c2e97493f",
+    "url": "/amiroous/react-ecommerce/index.html"
+  },
+  {
+    "revision": "b6bcfabeb3e9a18b5010",
+    "url": "/amiroous/react-ecommerce/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e5cd77718cdec0964829",
+    "url": "/amiroous/react-ecommerce/static/js/2.630e1165.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amiroous/react-ecommerce/static/js/2.630e1165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6bcfabeb3e9a18b5010",
+    "url": "/amiroous/react-ecommerce/static/js/main.4a992d28.chunk.js"
+  },
+  {
+    "revision": "de333909feea18f99f45",
+    "url": "/amiroous/react-ecommerce/static/js/runtime-main.217f500c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amiroous/react-ecommerce/static/media/logo.5d5d9eef.svg"
+  }
+]);
